c944c23d33ad4a2ee4966a36000a2b84a48e7088
Sahil Agashe <sahilagashe2002@gmail.com>
2025-04-11 23:05:21 +0530

    Initial Commit

---
+++ ARC-Menu-Animation/ARC_Menu_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARC_Menu_AnimationApp.swift
+//  ARC-Menu-Animation
+//
+//  Created by SAHIL AMRUT AGASHE on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ARC_Menu_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARC-Menu-Animation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARC-Menu-Animation
+//
+//  Created by SAHIL AMRUT AGASHE on 11/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
