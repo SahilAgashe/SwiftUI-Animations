b215f9ae119dc2cc935cbb704c20ab3439411b72
Sahil Agashe <sahilagashe2002@gmail.com>
2025-04-12 14:48:21 +0530

    SwiftUI ARC Menu Animation

--- ARC-Menu-Animation/ContentView.swift
+++ ARC-Menu-Animation/ContentView.swift
@@ -8,17 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Initial animation states
+    @State private var FABAngle = false
+    @State private var iconPosition = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            Text("SwiftUI Arc Menu")
+                .font(.largeTitle)
+            
+            ZStack {
+                Circle()
+                    .stroke()
+                    .frame(width: 150, height: 150)
+                    .opacity(0)
+                
+                Image(systemName: "circle.bottomhalf.fill")
+                    .font(.title)
+                    .offset(x: -75)
+                    .rotationEffect(.degrees(iconPosition ? 0 : -180))
+                    .animation(.easeInOut(duration: 1).delay(0.3), value: iconPosition)
+                
+                Image(systemName: "circle.dashed.inset.fill")
+                    .font(.title)
+                    .offset(x: -75)
+                    .rotationEffect(.degrees(iconPosition ? 30 : -180))
+                    .animation(.easeInOut(duration: 1).delay(0.2), value: iconPosition)
+                
+                Image(systemName: "target")
+                    .font(.title)
+                    .offset(x: -75)
+                    .rotationEffect(.degrees(iconPosition ? 60 : -180))
+                    .animation(.easeInOut(duration: 1).delay(0.1), value: iconPosition)
+                
+                Image(systemName: "circle.tophalf.fill")
+                    .font(.title)
+                    .offset(x: -75)
+                    .rotationEffect(.degrees(iconPosition ? 90 : -180))
+                    .animation(.easeInOut(duration: 1), value: iconPosition)
+            }
+            .clipShape(Rectangle().offset(x: -20, y: -50))
+            .offset(x: 150, y: 335)
+
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    ZStack {
+                        // FAB Icon
+                        Circle()
+                            .frame(width: 56, height: 56)
+                            .foregroundStyle(Color(.systemIndigo))
+                            .overlay {
+                                Image(systemName: "plus")
+                                    .rotationEffect(FABAngle ? .degrees(45) : .zero)
+                            }
+                            .animation(customAnimationWithTimingCurve, value: FABAngle)
+                            .shadow(color: .black, radius: 10, x: 1 , y: 1)
+                            .onTapGesture {
+                                FABAngle.toggle()
+                                iconPosition.toggle()
+                            }
+                        
+                    }
+                }.padding()
+            }
+        }// Main Z Stack
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
+
+
+private let easeInOutWithCustomDelay: Animation = .easeInOut.delay(0.25)
+private let customAnimationWithTimingCurve: Animation = .timingCurve(0.68, -0.6, 0.32, 1.6).delay(0.25)
